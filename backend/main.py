c48063fa42ed7004ce6d8eb43ada3c143e853bbf
Prathamesh <263696223+prathameshamone@users.noreply.github.com>
2026-07-07 11:50:59 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -701,6 +701,5 @@ def get_current_user(authorization: str = Header(None)):
 
 from routes.excel_report import router as excel_report_router   
 # from backend.routes.upload import router as upload_router    
-app = FastAPI()    
 app.include_router(excel_report_router)   
 # app.include_router(upload_router)
