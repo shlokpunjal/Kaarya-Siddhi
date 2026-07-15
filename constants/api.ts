d45a7cde83423691af6c569493e52f376926df1b
Mugdha Vyavhare <192891226+Mugdha008@users.noreply.github.com>
2026-07-15 11:39:53 +0530

    commit

--- constants/api.ts
+++ constants/api.ts
@@ -1,8 +1,4 @@
 
-<<<<<<< HEAD
 export const API_BASE_URL = "http://10.159.22.156:8000"; // Replace with your backend API base URL
-=======
-export const API_BASE_URL = "https://kaarya-siddhi.onrender.com"; // Replace with your backend API base URL
->>>>>>> 7034c24e5974851a1d001563f9b27c2a23c01f02
 
 
