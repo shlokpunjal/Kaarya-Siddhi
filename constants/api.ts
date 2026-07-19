d5b88810b67f05b95de97e1e90973b2fa0027ffa
Mugdha <192891226+Mugdha008@users.noreply.github.com>
2026-07-19 18:20:46 +0530

    Removed header from  the calendar pages and updated calendarSkeleton screen , also the tasks pages

--- constants/api.ts
+++ constants/api.ts
@@ -1,8 +1,4 @@
 
-<<<<<<< HEAD
 export const API_BASE_URL = "http://10.39.214.112:8000"; // Replace with your backend API base URL
-=======
-export const API_BASE_URL = "http://10.162.68.243:8000"; // Replace with your backend API base URL
->>>>>>> e913a0e03034c254532f62af1b2ee73b6855e60b
 
 
