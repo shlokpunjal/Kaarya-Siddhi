985d7684b9a020509788835df471f2678011f716
Kartik Gundla <207908131+kartikgundla@users.noreply.github.com>
2026-07-27 13:28:33 +0530

    Changed cloudinary files uploading logic

--- constants/api.ts
+++ constants/api.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://10.159.22.182:8000"; // Localhost for Android emulator
+export const API_BASE_URL = "https://kaarya-siddhi-0w7j.onrender.com"; // Localhost for Android emulator
 // https://kaarya-siddhi-0w7j.onrender.com
