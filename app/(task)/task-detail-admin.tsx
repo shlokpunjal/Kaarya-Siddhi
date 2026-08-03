fdbdbb0ce206babf5c27dff81a553320b961302d
Maheshwari242008 <mahiraccha24@gmail.com>
2026-08-03 12:04:04 +0530

    commit

--- app/(task)/task-detail-admin.tsx
+++ app/(task)/task-detail-admin.tsx
@@ -16,7 +16,7 @@ import { useTheme } from "../../context/ThemeContext";
 import { typography } from "../../theme/theme";
 import { wp, moderateScale } from "../../utils/responsive";
 import { useToast } from "../../context/ToastContext";
-
+import { AlertModal } from "../../components/common/AlertModal";
 import { authFetch } from "../../utils/authFetch";
 import TaskDetailSkeleton from "../../components/skeletonScreens/Tasks/TaskDetailSkeleton";
 import { ScreenHeader } from "../../components/task/ScreenHeader";
